a9f6c2e6820915ff8197023d95b7282f0b63932e
shahzod <shahzod.bokhidjonov@gmail.com>
2018-09-13 04:32:56 -0700

    Commit uploaded project

---
+++ myFirst/src/main/java/SpringBootStarter/Controller/TopicController.java
@@ -0,0 +1,41 @@
+package SpringBootStarter.Controller;
+
+import SpringBootStarter.Model.Topic;
+import SpringBootStarter.Service.TopicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TopicController {
+    @Autowired
+    private TopicService topicService;
+
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics(){
+        return topicService.getAllTopics();
+    }
+
+    @RequestMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable String id){
+        return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method= RequestMethod.POST, value = "/topics")
+    public void addT(@RequestBody Topic topic){
+        topicService.addTopic(topic);
+    }
+
+    @RequestMapping(method= RequestMethod.PUT, value = "/topics/{id}")
+    public void updateT(@RequestBody Topic topic, @PathVariable String id){
+        topicService.updateTopic(id,topic);
+    }
+
+    @RequestMapping(method= RequestMethod.DELETE, value = "/topics/{id}")
+    public void deleteT(@PathVariable String id){
+        topicService.deleteTopic(id);
+    }
+
+
+}
